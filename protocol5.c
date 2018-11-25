f5ad34549ccc0843930deafaaa27eee100fa61d0
David <david-nader@users.noreply.github.com>
2018-11-25 22:00:44 +0200

    Removed piggybacking of ACK
    Receiver now only sends ACK packets (no data packets), also
    Sender now only receives ACK packets

--- protocol5.c
+++ protocol5.c
@@ -21,23 +21,31 @@ static boolean between(seq_nr a, seq_nr b, seq_nr c) {
         return (false);
 }
 
-static void send_data(seq_nr frame_nr, seq_nr frame_expected, packet buffer[]) {
+static void send_data(seq_nr frame_nr, packet buffer[]) {
     /* Construct and send a data frame. */
     frame s;                                            /* scratch variable */
+    s.kind = data;
     s.info = buffer[frame_nr];                          /* insert packet into frame */
     s.seq = frame_nr;                                   /* insert sequence number into frame */
-    s.ack = (frame_expected + MAX_SEQ) % (MAX_SEQ + 1); /* piggyback ack */
+    // s.ack = (frame_expected + MAX_SEQ) % (MAX_SEQ + 1); /* piggyback ack */
     to_physical_layer(&s);                              /* transmit the frame */
     start_timer(frame_nr);                              /* start the timer running */
 }
 
+static void send_ack(seq_nr frame_nr) {
+    frame s;
+    s.kind = ack;
+    s.ack = frame_nr;
+    to_physical_layer(&s);
+}
+
 void protocol5(void) {
-    seq_nr next_frame_to_send;  /* MAX_SEQ > 1; used for outbound stream */
-    seq_nr ack_expected;        /* oldest frame as yet unacknowledged */
-    seq_nr frame_expected;      /* next frame_expected on inbound stream */
+    seq_nr next_frame_to_send;  /* (SENDER) MAX_SEQ > 1; used for outbound stream */
+    seq_nr ack_expected;        /* (SENDER) oldest frame as yet unacknowledged */
+    seq_nr frame_expected;      /* (RECEIVER) next frame_expected on inbound stream */
+    packet buffer[MAX_SEQ + 1]; /* (SENDER) buffers for the outbound stream */
+    seq_nr nbuffered;           /* (SENDER) number of output buffers currently in use */
     frame r;                    /* scratch variable */
-    packet buffer[MAX_SEQ + 1]; /* buffers for the outbound stream */
-    seq_nr nbuffered;           /* number of output buffers currently in use */
     seq_nr i;                   /* used to index into the buffer array */
     event_type event;
     ack_expected = 0;           /* next ack_expected inbound */
@@ -53,32 +61,39 @@ void protocol5(void) {
                     /* Accept, save, and transmit a new frame. */
                     from_network_layer(&buffer[next_frame_to_send]);       /* fetch new packet */
                     nbuffered = nbuffered + 1;                             /* expand the sender’s window */
-                    send_data(next_frame_to_send, frame_expected, buffer); /* transmit the frame */
+                    send_data(next_frame_to_send, buffer); /* transmit the frame */
                     inc(next_frame_to_send);                               /* advance sender’s upper window edge */
                 }
                 break;
             case frame_arrival:          /* a data or control frame has arrived */
                 from_physical_layer(&r); /* get incoming frame from physical layer */
-                if (r.seq == frame_expected)
-                {
-                    /* Frames are accepted only in order. */
-                    to_network_layer(&r.info); /* pass packet to network layer */
-                    inc(frame_expected);       /* advance lower edge of receiver’s window */
-                }
-                /* Ack n implies n - 1, n - 2, etc. Check for this. */
-                while (between(ack_expected, r.ack, next_frame_to_send))
-                {
-                    /* Handle piggybacked ack. */
-                    nbuffered = nbuffered - 1; /* one frame fewer buffered */
-                    stop_timer(ack_expected);  /* frame arrived intact; stop timer */
-                    inc(ack_expected);         /* contract sender’s window */
+                switch (r.kind) {
+                    case data:
+                        if (r.seq == frame_expected) {
+                            /* Frames are accepted only in order. */
+                            to_network_layer(&r.info); /* pass packet to network layer */
+                            send_ack(frame_expected);
+                            inc(frame_expected);       /* advance lower edge of receiver’s window */
+                        }
+                    break;
+                    case ack:
+                        /* Ack n implies n - 1, n - 2, etc. Check for this. */
+                        while (between(ack_expected, r.ack, next_frame_to_send)) {
+                            /* Handle piggybacked ack. */
+                            nbuffered = nbuffered - 1; /* one frame fewer buffered */
+                            stop_timer(ack_expected);  /* frame arrived intact; stop timer */
+                            inc(ack_expected);         /* contract sender’s window */
+                        }
+                    break;
+                    default:
+                        ; //do nothing, not reached
                 }
                 break;
             case timeout:                          /* trouble; retransmit all outstanding frames */
                 next_frame_to_send = ack_expected; /* start retransmitting here */
                 for (i = 1; i <= nbuffered; i++)
                 {
-                    send_data(next_frame_to_send, frame_expected, buffer); /* resend frame */
+                    send_data(next_frame_to_send, buffer); /* resend frame */
                     inc(next_frame_to_send);                               /* prepare to send the next one */
                 }
         }
